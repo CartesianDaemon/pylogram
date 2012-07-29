aaf19349752cedbbda52edf60d22e4025141274d
U-Red-Laptop\Jack <Jack@Red-Laptop.(none)>
2012-07-29 14:17:39 +0100

    Tweaked

--- example_lin_eq.py
+++ example_lin_eq.py
@@ -5,10 +5,6 @@ vars = pylogram.Varset()
 eq1 =    vars.a + vars.b   == 2
 eq2 =  2*vars.a + vars.b/3 == -7
 
-test =  5*vars.a +23 == 0
-#assert not test.evaluate(test)
-
 pylogram.constrain( eq1, eq2 )
 
-#assert test.evaluate(test)
 print( "a , b =", vars.a, ",", vars.b )
