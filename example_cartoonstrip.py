57727001d096510f5883926feff8adc8b1db93d3
U-Red-Laptop\Jack <Jack@Red-Laptop.(none)>
2012-07-31 09:29:50 +0100

    Trying .first syntax in cartoonstrip

--- example_cartoonstrip.py
+++ example_cartoonstrip.py
@@ -10,14 +10,15 @@ class Strip(Array):
     def __init__(self, N, width = 100, height=100):
         super().__init__(N,Panel)
         self.topleft = self.first.topleft = Point(padding,padding)
-        #constrain( self._arr[0].width * N == width )
-        constrain( self._arr[0].width * N == width )
+        self.first.width = width / N
+        #(self.first.width * N) [:]= width
         for a in self:
             self.panelwidth = a.width
             self.height = a.height = height # self.height = self.all.height
         for a,b in self.adj_objs():
             a.right = b.left
 
+display(Strip(1,300),w=400,h=300)
 print(Strip(1,300).sim_draw())
         
 # display(Strip(3,600, 200),w=650,h=250)
