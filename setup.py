493d9442025471dc3e76623bdf2a2693aad5bf24
U-Red-Laptop\Jack <Jack@Red-Laptop.(none)>
2012-08-13 17:00:44 +0100

    Added setup.py. Not sure it's correct.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Pylogram',
+    version='0.1.0',
+    author='Jack Vickeridge',
+    author_email='jackv@cantab.net',
+    packages=['pylogram'],
+    description='Automatic solutions to linear equations expressed in code, including simple geometric drawings. Based on Linogram by Mark Dominus.',
+    long_description=open('README').read(),
+)
