3c24db0055b5167ec4b9e03e21a980301502a1bb
U-Red-Laptop\Jack <Jack@Red-Laptop.(none)>
2012-08-14 22:30:15 +0100

    Updated version no.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pylogram',
-    version='0.1.0',
+    version='0.1.1',
     author='Jack Vickeridge',
     author_email='jackv@cantab.net',
     packages=['pylogram'],
